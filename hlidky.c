6add8607e51b308b3f2507adda03ab1d7b2cb4e1
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2019-02-26 21:20:33 +0100

    0.1

--- hlidky.c
+++ hlidky.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include "stdlib.h"
+#define BLOCK 16
 
 typedef struct {
     int to;
@@ -6,6 +8,14 @@ typedef struct {
 } edge;
 
 int main() {
+    int n, k, a, b, h;
+    edge **graph;
+
+    scanf("%d%d", &n, &k);
+    graph =calloc(n, sizeof(edge*));
+    for (; k > 0; k--) {
+        scanf("%d%d%d", &a, &b, &h);
+    }
     
     return 0;
 }
