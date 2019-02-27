5b105c7996e76371a9ed44b7ac2f78bf293a4f26
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2019-02-27 18:04:09 +0100

    0.1.2

--- hlidky.c
+++ hlidky.c
@@ -1,18 +1,35 @@
 #include <stdio.h>
-#include "stdlib.h"
-#define BLOCK 16
+#include <stdlib.h>
 
 typedef struct {
     int to;
     int watchCount;
 } edge;
 
+void addEdge(edge *vertex, int to, int count) {
+   while (vertex->to != -1) vertex++;
+   vertex->to = to;
+   vertex->watchCount = count;
+}
+
+void initGraph(edge **graph, int k) {
+    int i, j;
+
+    for (i = 0; i < k; i++) {
+        for (j = 0; j < (k - 1) ; j++) {
+            graph[i][j].to = -1;
+            graph[i][j].watchCount = -1;
+        }
+    }
+}
+
 int main() {
     int n, k, a, b, h;
     edge **graph;
 
     scanf("%d%d", &n, &k);
-    graph =calloc(n, sizeof(edge*));
+    graph = calloc(n, sizeof(edge*));
+    initGraph(graph, k);
     for (; k > 0; k--) {
         scanf("%d%d%d", &a, &b, &h);
     }
