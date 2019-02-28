39f69c7e9302b3a7cb844baf50a65a790d9676c8
prokopparuzek <prokop.paruzek@paruzkovi.cz>
2019-02-28 15:49:17 +0100

    0.2.3

--- hlidky.c
+++ hlidky.c
@@ -48,6 +48,14 @@ void findRoute(edge ** graph, buffer_t *queue) {
    printf("\n");
 }
 
+int solve(edge **graph, buffer_t *queue, int max) {
+    int back = 1;
+    while (back == 1) {
+        back = makeSteps(graph, queue, max);
+    }
+    return back;
+}
+
 int bts(edge **graph, int n, int a, int b) {
     int i;
     int back; // návratová hodnota solveStep
@@ -60,12 +68,12 @@ int bts(edge **graph, int n, int a, int b) {
         puts("Málo paměti");
         exit(1);
     }
-    memset(queue.parents, -1, n);
-    queue.stackAc[0] = b;
+    for (i = 0; i < n; i++) queue.parents[i] = -1;
+    queue.stackAc[0] = a;
 
     for (i = 0;; i++) {
-        back = makeSteps(graph, &queue, i);
-        if (back == 1) {
+        back = solve(graph, &queue, i);
+        if (back == 0) {
             findRoute(graph, &queue);
             printf("%d\n", i);
         }
@@ -73,36 +81,43 @@ int bts(edge **graph, int n, int a, int b) {
 }
 
 int makeSteps(edge **graph, buffer_t *queue, int max) {
-    unsigned int i, *swapS;
+    unsigned int i, *swapS, test = 0;
     int back; // návratová hodnota solveStep
 
     for (i = 0; i <= queue->indexAc; i++) {
         back = solveStep(graph, queue, i, max);
-        if (back == 1) {
-            return 1;
+        if (back == 0) {
+            return 0;
+        }
+        if (back != 2) {
+            test = 1;
         }
     }
-    swapS = queue->stackAc;
-    queue->stackAc = queue->stackFu;
-    queue->stackFu = swapS;
-    queue->indexAc = queue->indexFu;
-    queue->indexFu = -1;
-    return 0;
+    if (test == 1) {
+        swapS = queue->stackAc;
+        queue->stackAc = queue->stackFu;
+        queue->stackFu = swapS;
+        queue->indexAc = queue->indexFu;
+        queue->indexFu = -1;
+        return 1;
+    }
+    return 2;
 }
 
 int solveStep(edge **graph, buffer_t *queue, int index, int max) {
-    int i;
+    int i, test = 0;
     for (i = 0; graph[index][i].to != -1; i++) {
-        if (graph[index][i].watchCount <= max || queue->parents[graph[index][i].to] == -1) {
+        if (graph[index][i].watchCount <= max || queue->parents[graph[index][i].to] != -1) {
+            test = 1;
             queue->indexFu++; // zvyš index příštích bodů o 1
             queue->stackFu[queue->indexFu] = graph[index][i].to; // ulož tam aktuální bod
             queue->parents[graph[index][i].to] = queue->stackAc[queue->indexAc];
             if (graph[index][i].to == queue->b) {
-                return 1;
+                return 0;
             }
         }
     }
-    return 0;
+    return test?1:2;
 }
 
 int main() {
